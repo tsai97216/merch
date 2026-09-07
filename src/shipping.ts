fbf00e875fd856741f097f1510c72f067c511fef
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-09-07 20:44:07 +0800

    refactor: keep item detail shipping on app entry

--- src/shipping.ts
+++ src/shipping.ts
@@ -1,5 +1,4 @@
 import './shipping.css';
-import './item-detail-shipping';
 import { getStore, type MerchStore } from './store';
 import type { Item, ShippingRecord } from './types';
 import { escapeHtml, qs } from './utils/dom';
