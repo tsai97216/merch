6c726991c4d53313b56771a4ae6284f6aaa38019
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-09-10 19:21:40 +0800

    fix: load dedicated statistics mobile styles

--- src/statistics.ts
+++ src/statistics.ts
@@ -1,4 +1,5 @@
 import './statistics.css';
+import './statistics-mobile.css';
 import { getStore, type MerchStore } from './store';
 import { aggregateStatistics, statisticsYears, type Chart, type WorkItemDetail } from './statistics-data';
 import { currentYear } from './utils/date';
